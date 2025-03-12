50212fde6fe358f9ef5697c427ae5d7acf4d9b08
1 <a1@MacBook-Pro-3.local>
2025-03-12 10:21:45 +0700

    updated routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,13 @@ import { Final } from './Final'
 import { PATH_PREFIX } from './constants'
 
 function App() {
-  const rebusWord = import.meta.env.REBUS_WORD || '777'
+  const rebusWord = import.meta.env.REBUS_WORD || 'qwe'
   console.log({ rebusWord, PATH_PREFIX })
 
   return (
     <Routes>
-      <Route path={PATH_PREFIX} element={<Rebus />} >
+      <Route path={PATH_PREFIX}  >
+        <Route index element={<Rebus />} />
         <Route path={rebusWord} element={<Final />} />
       </Route>
     </Routes>
