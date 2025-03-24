7a242397d9ceb8e73dead45f35d0bac284fbc949
BANNED <maximjaks@gmail.com>
2025-03-24 23:09:44 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,11 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
-import { PATH_PREFIX } from "./constants.ts";
+//import { PATH_PREFIX } from "./constants.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename={"/"}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
