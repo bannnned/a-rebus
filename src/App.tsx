0b67fd8e737f06f26c4248173ac3fcc794016805
BANNED <maximjaks@gmail.com>
2025-03-24 15:15:12 +0300

    changed consts

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import { Route, Routes } from 'react-router'
-import './App.css'
-import { Rebus } from './Rebus'
-import { Final } from './Final'
-import { PATH_PREFIX } from './constants'
+import { Route, Routes } from "react-router";
+import "./App.css";
+import { Rebus } from "./Rebus";
+import { Final } from "./Final";
+import { PATH_PREFIX } from "./constants";
 
 function App() {
-  const rebusWord = import.meta.env.REBUS_WORD || 'react-4-life'
+  const rebusWord = import.meta.env.REBUS_WORD || "react-4-life";
+  console.log({ rebusWord });
 
   return (
     <Routes>
-      <Route path={PATH_PREFIX}  >
+      <Route path={PATH_PREFIX}>
         <Route index element={<Rebus />} />
         <Route path={rebusWord} element={<Final />} />
       </Route>
       <Route path="*" element={<div>Не та дорога, путник</div>} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
